c37abbb0d032679a7084e4e683ec9293b841ae05
Chenyi Cai <caichenyi0609@gmail.com>
2020-11-18 11:28:18 +0800

    add wallwindow constructor

--- src/cube/generator/elements/WallWithDoor.java
+++ src/cube/generator/elements/WallWithDoor.java
@@ -1,10 +1,20 @@
 package cube.generator.elements;
 
+import wblut.geom.WB_Point;
 import wblut.processing.WB_Render3D;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class WallWithDoor implements Element{
+
+    int pts_num;
+    ArrayList<WB_Point> rans_pts_on;
+
+    public WallWithDoor(int pts_num) {
+        this.pts_num = pts_num;
+    }
+
     @Override
     public void saveToCsv(String path) throws IOException {
 
